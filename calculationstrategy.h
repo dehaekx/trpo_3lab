9bb2e9f1ce2b2d08c7ae60e96c9fbb63392e69dd
Alexey Seren <dehaekx@gmail.com>
2024-06-07 15:10:54 +0700

    add comments which explain interface and add const to methods

--- calculationstrategy.h
+++ calculationstrategy.h
@@ -8,6 +8,13 @@
 #include <QFileInfo>
 #include <QFileInfoList>
 
+/*
+ * Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых версий
+ * некоторого алгоритма.
+ *
+ * Контекст использует этот интерфейс для вызова алгоритма, определённого
+ * Конкретными Стратегиями.
+ */
 class CalculationStrategy
 {
 public:
@@ -22,7 +29,7 @@ class Folder_CalculationStrategy : public CalculationStrategy
 {
 public:
     ~Folder_CalculationStrategy() = default;
-     void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const = 0;
+    void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const override;
 
 private:
     qint64 CountFolder(const QString& path);
@@ -36,7 +43,7 @@ class Type_CalculationStrategy : public CalculationStrategy
 {
 public:
     ~Type_CalculationStrategy() = default;
-     void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const = 0;
+    void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const override;
 
 private:
     void CountFileType(const QString &path, QMap<QString, qint64>& size) const;
