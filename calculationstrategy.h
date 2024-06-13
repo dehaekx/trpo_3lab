35cb17dca91ec3c32f6e7c3c8c839f59ef1caf85
Alexey Seren <dehaekx@gmail.com>
2024-06-13 13:51:23 +0700

    change places fucntion

--- calculationstrategy.h
+++ calculationstrategy.h
@@ -21,24 +21,4 @@ public:
     virtual void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const = 0;
 };
 
-// class Folder_CalculationStrategy : public CalculationStrategy
-// {
-// public:
-//     ~Folder_CalculationStrategy() = default;
-//     void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const override;
-
-// private:
-//     qint64 CountFolder(const QString& path) const;
-// };
-
-// class Type_CalculationStrategy : public CalculationStrategy
-// {
-// public:
-//     ~Type_CalculationStrategy() = default;
-//     void calculationMethod(const QString &path, QMap<QString, qint64> &cont) const override;
-
-// private:
-//     void CountFileType(const QString &path, QMap<QString, qint64>& size) const;
-// };
-
 #endif // CALCULATIONSTRATEGY_H
--- main.cpp
+++ main.cpp
@@ -2,8 +2,52 @@
 #include <QDebug>
 #include <QDir>
 #include <QMap>
-#include <QMapIterator>
 #include "context.h"
+#include "folder_strategy.h"
+#include "type_strategy.h"
+
+void print_Map(const QMap<QString, qint64> &map);
+void print_Map(const QMap<QString, QString> *map);
+QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& size, int strategy);
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QString currentReposiroty = QDir::currentPath().section("/", 0, -2);
+    qDebug() << "Direction: " << currentReposiroty << Qt::endl;
+    //qDebug() << currentReposiroty << Qt:: endl;
+    QString info = currentReposiroty + "/tests";
+
+    // /tests                  - не пустая + содержит вложения
+    // /tests/test1_repository - содержит папки
+    // /tests/test2_repository - пустая
+    // /tests/test3_repository - не пустая, но с файлами, имеющие развер 0
+    // /tests/test4_repository - содержит вложения
+
+    // Использование контекста со стратегией Folder_CalculationStrategy
+    context ctx(std::make_shared<Folder_CalculationStrategy>());
+    ctx.fill_Map(info);
+    qDebug() << "Folders:" << Qt::endl;
+    print_Map(ctx.get_Map());
+    qDebug() << "Folder Percentages:" << Qt::endl;
+    print_Map(CountVolumePercent(ctx.get_Map(), 1));
+
+    qDebug() << Qt::endl << "Change strategy" << Qt::endl;
+
+    // Изменение стратегии на Type_CalculationStrategy
+    ctx.set_Strategy(std::make_shared<Type_CalculationStrategy>());
+    ctx.fill_Map(info);
+    qDebug() << "Types:" << Qt::endl;
+    print_Map(ctx.get_Map());
+    qDebug() << "Type Percentages:" << Qt::endl;
+    print_Map(CountVolumePercent(ctx.get_Map(), 2));
+
+    return a.exec();
+}
+
+
+
+
 
 void print_Map(const QMap<QString, qint64> &map)
 {
@@ -27,20 +71,21 @@ void print_Map(const QMap<QString, QString> *map)
     }
 }
 
-QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& size, int strategy)
+
+QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& cont, int strategy)
 {
     float accuracy = 0.01;
     // Инициализация переменных
     float total = 0;
     bool flag = false;
     QMap<QString, QString>* map = new QMap<QString, QString>;
-    if (!map || size.isEmpty())
+    if (!map || cont.isEmpty())
     {
         return map;
     }
 
     // Подсчёт общего количества
-    for (auto value : size)
+    for (auto value : cont)
     {
         total += value;
     }
@@ -50,15 +95,15 @@ QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& size, in
     {
         if (total == 0)
         {
-            for (auto it = size.constBegin(); it != size.constEnd(); ++it)
+            for (auto it = cont.constBegin(); it != cont.constEnd(); ++it)
             {
-                map->insert(QString("*.") + it.key(), QString("0.00 %"));
+                map->insert(QString(".") + it.key(), QString("0.00 %"));
             }
             return map;
         }
 
         float others = 0;
-        for (auto it = size.constBegin(); it != size.constEnd(); ++it)
+        for (auto it = cont.constBegin(); it != cont.constEnd(); ++it)
         {
             float percent = it.value() / total;
             if (percent < accuracy)
@@ -68,7 +113,7 @@ QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& size, in
             }
             else
             {
-                map->insert(QString("*.") + it.key(), QString::number(percent * 100, 'f', 2) + " %");
+                map->insert(QString(".") + it.key(), QString::number(percent * 100, 'f', 2) + " %");
             }
         }
         if (flag)
@@ -83,14 +128,14 @@ QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& size, in
             map->insert("Current path", "0.00 %");
             return map;
         }
-        for (auto it = size.constBegin(); it != size.constEnd(); ++it)
+        for (auto it = cont.constBegin(); it != cont.constEnd(); ++it)
         {
             float percent = it.value() / total;
             if (percent == 0)
             {
                 map->insert(it.key(), "0.00 %");
             }
-            else if (percent * 100 < accuracy)
+            else if (percent < accuracy)
             {
                 map->insert(it.key(), QString("%1 %").arg(QString::number(accuracy)));
             }
@@ -102,44 +147,4 @@ QMap<QString, QString>* CountVolumePercent(const QMap<QString, qint64>& size, in
     }
 
     return map;
-}
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    QString currentReposiroty = QDir::currentPath().section("/", 0, -2);
-    qDebug() << "Direction: " << currentReposiroty << Qt::endl;
-    //qDebug() << currentReposiroty << Qt:: endl;
-    QString info = currentReposiroty + "/tests/test4_repository";
-
-    // /tests                  - не пустая + содержит вложения
-    // /tests/test1_repository - содержит папки
-    // /tests/test2_repository - пустая
-    // /tests/test3_repository - не пустая, но с файлами, имеющие развер 0
-    // /tests/test4_repository - содержит вложения
-
-
-
-
-
-
-    // Использование контекста со стратегией Folder_CalculationStrategy
-    context ctx(std::make_shared<Folder_CalculationStrategy>());
-    ctx.fill_Map(info);
-    qDebug() << "Folders:" << Qt::endl;
-    print_Map(ctx.get_Map());
-    qDebug() << "Folder Percentages:" << Qt::endl;
-    print_Map(CountVolumePercent(ctx.get_Map(), 1));
-
-    qDebug() << Qt::endl << "Change strategy" << Qt::endl;
-
-    // Изменение стратегии на Type_CalculationStrategy
-    ctx.set_Strategy(std::make_shared<Type_CalculationStrategy>());
-    ctx.fill_Map(info);
-    qDebug() << "Types:" << Qt::endl;
-    print_Map(ctx.get_Map());
-    qDebug() << "Type Percentages:" << Qt::endl;
-    print_Map(CountVolumePercent(ctx.get_Map(), 2));
-
-    return a.exec();
 }
