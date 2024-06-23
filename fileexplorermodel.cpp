80e6eb5cb578b15d7d8abc20b348808e33eb4d24
Alexey Seren <dehaekx@gmail.com>
2024-06-23 14:17:40 +0700

    реализовал уже конечную модель

--- fileexplorermodel.cpp
+++ fileexplorermodel.cpp
@@ -1,6 +1,75 @@
 #include "fileexplorermodel.h"
 
-FileExplorerModel::FileExplorerModel(QObject *parent)
+FileExplorerModel::FileExplorerModel(QObject *parent, QMap<QString, QString> *map_): QAbstractItemModel(parent)
 {
+    map = *map_;
+}
 
+QModelIndex FileExplorerModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if (hasIndex(row, column, parent))
+    {
+        return createIndex(row, column);
+    }
+    return QModelIndex();
+}
+
+QModelIndex FileExplorerModel::parent(const QModelIndex &child) const { Q_UNUSED(child) return QModelIndex(); }
+
+int FileExplorerModel::rowCount(const QModelIndex &par) const{ Q_UNUSED(par) return map.size(); }
+
+int FileExplorerModel::columnCount(const QModelIndex &par) const { Q_UNUSED(par) return 2;}
+
+QVariant FileExplorerModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole)
+    {
+        return QVariant();
+    }
+    if (orientation == Qt::Vertical)
+    {
+        return section;
+    }
+    if  (section == 0)
+        return QString("NAME");
+    else if (section == 1)
+        return QString("PERCENT");
+    return QVariant();
+}
+
+QVariant FileExplorerModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || map.size() <= index.row() || (role != Qt::DisplayRole && role != Qt::EditRole) || map.isEmpty())
+    {
+        return QVariant();
+    }
+    QMapIterator<QString, QString> i(map);
+    // Перемещение итератора на нужную позицию
+    int k = 0;
+    i.next();
+    while (i.hasNext() && k < index.row())
+    {
+        i.next();
+        k++;
+    }
+    if (index.column() == 0)
+    {
+        return i.key();
+    }
+    else if (index.column() == 1)
+    {
+        return i.value();
+    }
+}
+
+void FileExplorerModel::updmapSlot(const QMap<QString, QString>* map_)
+{
+    beginResetModel();  // помогла при выводе не все файлы выводились
+    if(map_)
+    {
+        qDebug() << "SFDPODPOCKSPOCKSPODKCPOSKDCOPSKDCKSDPCOSD";
+        map = *map_;
+        //qDebug() << map;
+    }
+    endResetModel();
 }
--- fileexplorermodel.h
+++ fileexplorermodel.h
@@ -4,19 +4,25 @@
 #include <QFileSystemModel>
 #include <QObject>
 #include <QMap>
+#include <QDebug>
 
 // QFileSystemModel – модель для работы с файлами и папками на локальном компьютере;
 
-class FileExplorerModel: public QFileSystemModel
+class FileExplorerModel: public QAbstractItemModel
 {
     Q_OBJECT
+public slots:
+    void updmapSlot(const QMap<QString, QString>* map_);
 public:
-    FileExplorerModel(QObject *parent = nullptr);
-    // int rowCount();
-    // int columnCount();
-    // QVariant headerData();
-    // QVariant setData();
-
+    FileExplorerModel(QObject *parent = nullptr, QMap<QString, QString>* map_ = nullptr);
+    QModelIndex index(int row, int column, const QModelIndex &parent) const override;
+    QModelIndex parent(const QModelIndex &child) const override;
+    virtual int rowCount(const QModelIndex &parent) const override;
+    virtual int columnCount(const QModelIndex &parent) const override;
+    virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    virtual QVariant data(const QModelIndex &index, int rool) const override;
+private:
+    QMap<QString, QString> map;
 };
 
 #endif // FILEEXPLORERMODEL_H
